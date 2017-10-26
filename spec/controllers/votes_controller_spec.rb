98ad957752fb707d35bbb1a3dccca959bfe717e2
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-10-26 18:26:25 -0400

    Checkpoint 29 testing failure

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -96,4 +96,20 @@ include SessionsHelper
        end
      end
    end
+
+   describe "#create_vote" do
+     it "sets post up_votes to 1 " do
+       expect(post.up_votes).to eq(1)
+     end
+
+     it "calls #create_vote when a post is created" do
+       post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) 
+       expect(post).to receive(:create_vote)
+       post.save
+     end
+
+     it "associates the vote with post owner" do
+       expect(post.votes.first.user).to eq(post.user)
+     end
+   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -58,7 +58,6 @@ RSpec.describe Post, type: :model do
      end
 
      describe "#update_rank" do
- # #28
        it "calculates the correct rank" do
          post.update_rank
          expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
@@ -74,6 +73,22 @@ RSpec.describe Post, type: :model do
          old_rank = post.rank
          post.votes.create!(value: -1, user: user)
          expect(post.rank).to eq (old_rank - 1)
+       end
+     end
+
+     describe "#create_vote" do
+       it "sets post up_votes to 1 " do
+         expect(post.up_votes).to eq(1)
+       end
+
+       it "calls #create_vote when a post is created" do
+         post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+         expect(post).to receive(:create_vote)
+         post.save
+       end
+
+       it "associates the vote with post owner" do
+         expect(post.votes.first.user).to eq(post.user)
        end
      end
    end
